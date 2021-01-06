cc9074142422c6375f4264d1b6696413b6682c20
TomasJP95 <tomasjimenezponce@gmail.com>
2021-01-06 20:04:01 +0100

    Parte front añadida para los subcomités

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,6 +84,104 @@
             </div>
         </div>
 
+        {{-- SUBCOMITÉS --}}
+        <div class="card">
+            <div class="card-header bg-dark">
+                <h3 class="card-title">Subcomités de cada comité</h3>
+            </div>
+            <div class="card-body pb-0">
+                <div class="row">
+                    <div class="col-lg-4 col-sm-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-light elevation-1"><i class="fas fa-user-tie"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Presidencia</span>
+                                <span class="info-box-number">
+                                    {{\App\Subcomittee::subcomitte_presidencia()}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-light elevation-1"><i class="fas fa-file-signature"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Secretaría</span>
+                                <span class="info-box-number">
+                                    {{\App\Subcomittee::subcomitte_secretaria()}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-light elevation-1"><i class="fas fa-list-ol"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Programa</span>
+                                <span class="info-box-number">
+                                    {{\App\Subcomittee::subcomitte_programa()}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-light elevation-1"><i class="fas fa-people-carry"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Igualdad</span>
+                                <span class="info-box-number">
+                                    {{\App\Subcomittee::subcomitte_igualdad()}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-light elevation-1"><i class="fas fa-piggy-bank"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Sostenibilidad</span>
+                                <span class="info-box-number">
+                                    {{\App\Subcomittee::subcomitte_sostenibilidad()}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-light elevation-1"><i class="fas fa-euro-sign"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Finanzas</span>
+                                <span class="info-box-number">
+                                    {{\App\Subcomittee::subcomitte_finanzas()}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-light elevation-1"><i class="fas fa-user-check"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Logística</span>
+                                <span class="info-box-number">
+                                    {{\App\Subcomittee::subcomitte_logistica()}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+                        <div class="info-box">
+                        <span class="info-box-icon bg-light elevation-1"><i class="fas fa-users"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Comunicación</span>
+                                <span class="info-box-number">
+                                    {{\App\Subcomittee::subcomitte_comunicacion()}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 
@@ -137,7 +235,7 @@
                             <div class="info-box-content">
                                 <span class="info-box-text">Presidencia</span>
                                 <span class="info-box-number">
-                                    {{\App\Meeting::meetings_presidencia()->count()}}
+                                    {{\App\Meeting::meetings_presidencia()}}
                                 </span>
                             </div>
                         </div>
@@ -148,7 +246,7 @@
                             <div class="info-box-content">
                                 <span class="info-box-text">Secretaría</span>
                                 <span class="info-box-number">
-                                    {{\App\Meeting::meetings_secretaria()->count()}}
+                                    {{\App\Meeting::meetings_secretaria()}}
                                 </span>
                             </div>
                         </div>
@@ -159,7 +257,7 @@
                             <div class="info-box-content">
                                 <span class="info-box-text">Programa</span>
                                 <span class="info-box-number">
-                                    {{\App\Meeting::meetings_programa()->count()}}
+                                    {{\App\Meeting::meetings_programa()}}
                                 </span>
                             </div>
                         </div>
@@ -170,7 +268,7 @@
                             <div class="info-box-content">
                                 <span class="info-box-text">Igualdad</span>
                                 <span class="info-box-number">
-                                    {{\App\Meeting::meetings_igualdad()->count()}}
+                                    {{\App\Meeting::meetings_igualdad()}}
                                 </span>
                             </div>
                         </div>
@@ -181,7 +279,7 @@
                             <div class="info-box-content">
                                 <span class="info-box-text">Sostenibilidad</span>
                                 <span class="info-box-number">
-                                    {{\App\Meeting::meetings_sostenibilidad()->count()}}
+                                    {{\App\Meeting::meetings_sostenibilidad()}}
                                 </span>
                             </div>
                         </div>
@@ -192,7 +290,7 @@
                             <div class="info-box-content">
                                 <span class="info-box-text">Finanzas</span>
                                 <span class="info-box-number">
-                                    {{\App\Meeting::meetings_finanzas()->count()}}
+                                    {{\App\Meeting::meetings_finanzas()}}
                                 </span>
                             </div>
                         </div>
@@ -203,18 +301,18 @@
                             <div class="info-box-content">
                                 <span class="info-box-text">Logística</span>
                                 <span class="info-box-number">
-                                    {{\App\Meeting::meetings_logistica()->count()}}
+                                    {{\App\Meeting::meetings_logistica()}}
                                 </span>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-6 col-sm-12">
                         <div class="info-box">
-                            <span class="info-box-icon bg-light elevation-1"><i class="fas fa-users"></i></span>
+                        <span class="info-box-icon bg-light elevation-1"><i class="fas fa-users"></i></span>
                             <div class="info-box-content">
                                 <span class="info-box-text">Comunicación</span>
                                 <span class="info-box-number">
-                                    {{\App\Meeting::meetings_comunicacion()->count()}}
+                                    {{\App\Meeting::meetings_comunicacion()}}
                                 </span>
                             </div>
                         </div>
